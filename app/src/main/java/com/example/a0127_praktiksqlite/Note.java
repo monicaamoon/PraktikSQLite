7f61e9a55e5626d0127ab418b02d2a46082256ad
monicaamoon <monica.0758@students.amikom.ac.id>
2020-12-21 11:11:15 +0700

    moon

---
+++ app/src/main/java/com/example/a0127_praktiksqlite/Note.java
@@ -0,0 +1,31 @@
+package com.example.a0127_praktiksqlite;
+
+public class Note {
+    private int id;
+    private String title;
+    private String note;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
